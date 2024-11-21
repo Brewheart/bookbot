3ed75a83495c6587e2195b2075d406f79471f769
Brewheart <ithjaltarna@gmail.com>
2024-11-21 12:19:54 +0100

    updated main.py to print number of words in frankenstein.txt

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    num_words = get_book_words(text)
+    print(f"{num_words} words were found in the text")
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+    
+def get_book_words(text):
+    words = text.split()
+    return len(words)
+
 
 if __name__ == "__main__":
     main()
